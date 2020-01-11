dd0b62df6916d0c4b67ee93f021354b087bca449
Eric035 <ericleung1997@gmail.com>
2020-01-11 12:41:46 -0500

    new repo

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Animator anim;
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Open door\n");
+            anim.SetInteger("Opened", 1);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Animator animator;
+
+    void FixedUpdate()
+    {
+        int moveHorizontal = (int) Input.GetAxis("Horizontal");
+        int moveVertical = (int) Input.GetAxis("Vertical");
+
+        Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+
+        float newVelocityX = 0f;
+        float newVelocityY = 0f;
+
+        if (moveHorizontal < 0 && currentVelocity.x <= 0)
+        {
+            newVelocityX = -speed;
+            animator.SetInteger("DirectionX", -1);
+        }
+        else if (moveHorizontal > 0 && currentVelocity.x >= 0)
+        {
+            newVelocityX = speed;
+            animator.SetInteger("DirectionX", 1);
+        }
+        else
+        {
+            animator.SetInteger("DirectionX", 0);
+        }
+
+
+
+        if (moveVertical < 0 && currentVelocity.y <= 0)
+        {
+            newVelocityY = -speed;
+            animator.SetInteger("DirectionY", -1);
+        }
+        else if (moveVertical > 0 && currentVelocity.y >= 0)
+        {
+            newVelocityY = speed;
+            animator.SetInteger("DirectionY", 1);
+        }
+        else
+        {
+            animator.SetInteger("DirectionY", 0);
+            
+        }
+
+
+        if (Math.Abs(moveHorizontal) >= Math.Abs(moveVertical))
+        {
+            newVelocityY = 0;
+
+        } else if(Math.Abs(moveHorizontal) <= Math.Abs(moveVertical))
+        {
+            newVelocityX = 0;   
+
+        }
+       
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(newVelocityX, newVelocityY);
+
+    }
+}
