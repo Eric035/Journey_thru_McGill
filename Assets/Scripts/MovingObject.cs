1855dc07d3202f02415ddad16fe17201bf78c500
David Ivanov <ivanovd1998@gmail.com>
2020-01-11 18:51:02 -0500

    Slime boi ready

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class MovingObject : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private bool isMoving = false;
+    public float range = 0;
+    private float nextX;
+    private float nextY;
+
+    private float moveTime;
+    public float startMoveTime;
+
+    void Start()
+    {
+        moveTime = startMoveTime;
+    }
+
+    void FixedUpdate()
+    {
+        if (!isMoving)
+        {
+            
+            nextX = Random.Range(-range, range);
+            nextY = Random.Range(-range , range);
+            moveTime = startMoveTime;
+            isMoving = true;
+        }
+        else
+        {
+            if(moveTime <= 0)
+            {
+                isMoving = false;
+            } else
+            {
+                moveTime -= Time.deltaTime;
+                
+            }
+        }
+
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(nextX, nextY);
+    }
+}
